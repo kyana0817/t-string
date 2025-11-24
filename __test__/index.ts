e047c1fa2e28bc2864eea76b25e157af43fc3b1b
kyana <kyana0817@gmail.com>
2025-11-24 12:47:54 +0900

    :tada: initial commit

--- __test__/index.ts
+++ __test__/index.ts
@@ -1,7 +1,35 @@
+import { describe } from 'node:test';
 import { it, expect } from 'vitest';
+import { t, ValidationError } from '../src/index.js';
 
 
-it('hello', () => {
-  expect('hello')
-    .equal('hello');
+describe('simple t-string', () => {
+  it('single template string', () => {
+    const _t = t`Hello, ${'text'}!`;
+    const result = _t.str({ text: 'World' });
+    expect(result).toBe('Hello, World!');
+  });
+
+  it('multiple keys template string', () => {
+    const _t = t`Sum of ${0} and ${1} is ${'result'}.`;
+    const result = _t.str(2, 3, { result: 5 });
+    expect(result).toBe('Sum of 2 and 3 is 5.');
+  });
+});
+
+describe('t-string with validation', () => {
+  it('validates number inputs', () => {
+    const _t = t`Age: ${'age'}`.validate({
+      age: (value) => typeof value === 'number' && value >= 0,
+    });
+    const result = _t.str({ age: 25 });
+    expect(result).toBe('Age: 25');
+  });
+  it('throws error on invalid input', () => {
+    const _t = t`Age: ${'age'}`.validate({
+      age: (value) => typeof value === 'number' && value >= 0,
+    });
+    expect(() => _t.str({ age: -5 }))
+      .toThrowError(ValidationError);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,52 @@
-const test: string = 'Hello World';
-console.log(test);
+
+export type Key = string | number;
+
+export type ValidateFuncs<T extends Array<Key>> = {
+  [K in T[number]]: (value: string | number) => boolean;
+};
+
+export type ValidatorMap<T extends Array<Key>> = Record<
+  T[number],
+  (value: string | number) => boolean
+>;
+
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ValidationError';
+  }
+}
+
+export function t<T extends Array<Key>>(
+  strings: TemplateStringsArray,
+  ...keys: T
+) {
+  const _validaters = {} as ValidatorMap<T>;
+  
+  for (const k of keys) {
+    _validaters[k as T[number]] = () => true;
+  }
+
+  return {
+    validate(validaters: Partial<ValidateFuncs<T>>){
+      for (const [k, v] of Object.entries(validaters)) {
+        _validaters[k as T[number]] = v as (value: string | number) => boolean;
+      }
+      return this;
+    },
+    str(...values: (number | string| Record<string, any>)[]) {
+      const dict = values[values.length - 1] || {};
+      const result = [strings[0]];
+      keys.forEach((key, i) => {
+        // @ts-expect-error: index signature
+        const value = Number.isInteger(key) ? values[key] : dict[key];
+        const validate = _validaters[key as T[number]];
+        if (!validate(value)) {
+          throw new ValidationError(`Invalid value for key "${key}": ${value}`);
+        }
+        result.push(value, strings[i + 1]);
+      });
+      return result.join('');
+    }
+  };
+}
